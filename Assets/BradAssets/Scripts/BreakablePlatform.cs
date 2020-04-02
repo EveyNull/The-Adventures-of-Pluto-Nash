d46614aa77a4f4b3502f0bdf8ec5c44d5e02b1d5
Unknown <king.brad1997@gmail.com>
2020-04-02 17:49:23 +0100

    Brad's Assets
    Added some of the more relevant assets, hopefully there'll be some use in them

---
+++ Assets/BradAssets/Scripts/BreakablePlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakablePlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+
+            StartCoroutine(DestroyPlatform());
+        }
+    }
+
+    IEnumerator DestroyPlatform()
+    {
+        yield return new  WaitForSeconds(1);
+        
+        Destroy(this.gameObject);
+    }
+}
