5866d521fcb54e9a86e9102b5942d77cd72c6cdc
aravash2 <Jacob2.Woodman@live.uwe.ac.uk>
2020-04-10 14:17:40 +0100

    respawning collectables

---
+++ Assets/FinalAssets/scripts/respawner.cs
@@ -0,0 +1,46 @@
+﻿/**
+ * Created by Jacob Woodman 10/04/2020
+ * Designed to respawn power ups some time after collection
+ * */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawner : MonoBehaviour
+{
+    public GameObject item;
+    public float respawnTime = 13;
+    private bool collected = false;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.CompareTag("Player") || other.CompareTag("enemy")) && !collected)
+        {
+            Debug.Log("calling respawn");
+            StartCoroutine(countdown());
+        }
+    }
+
+    private IEnumerator countdown()
+    {
+        collected = true;
+        yield return new WaitForSeconds(respawnTime);
+        spawn();
+    }
+
+    void spawn()
+    {
+        GameObject spawnedItem = Instantiate(item, transform.position, transform.rotation);
+        spawnedItem.transform.SetParent(transform);
+        collected = false;
+    }
+}
