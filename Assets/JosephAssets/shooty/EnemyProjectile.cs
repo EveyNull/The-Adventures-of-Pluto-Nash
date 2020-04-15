ef910ace9327f947dcf1b75ab2f6006dc83e1fde
joe <joseph2.garrad@live.uwe.ac.uk>
2020-04-15 17:46:35 +0100

    projectile enemy addded

---
+++ Assets/JosephAssets/shooty/EnemyProjectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public GameObject PROJ;
+    public float timer = 0;
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        timer += Time.deltaTime; 
+        if(timer >= 5)
+        {
+            Instantiate(PROJ, transform.position, transform.rotation);
+            timer = 0;
+        }
+    }
+}
---
+++ Assets/JosephAssets/shooty/PROJmovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PROJmovement : MonoBehaviour
+{
+    GameObject player;
+    public int Speed = 5;
+    Vector3 PlayerPos;
+    public bool KeepGoing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerPos = new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z);
+
+       
+
+        if (KeepGoing == false)
+        {
+            transform.LookAt(PlayerPos);
+            towardsPlayer();
+        }
+        if(KeepGoing == true)
+        {
+            Foward();
+        }
+    }
+    void towardsPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, PlayerPos, Speed * Time.deltaTime);
+    }
+    void Foward()
+    {
+        transform.position += transform.forward * Speed * Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            KeepGoing = true;
+        }
+    }
+}
