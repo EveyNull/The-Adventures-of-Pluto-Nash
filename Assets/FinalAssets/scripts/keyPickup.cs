68a6da3c7adf98052d83d6a98bdcf76d0664c27c
aravash2 <Jacob2.Woodman@live.uwe.ac.uk>
2020-04-17 18:24:52 +0100

    added working lock and key

---
+++ Assets/FinalAssets/scripts/keyPickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyPickup : MonoBehaviour
+{
+    public GameObject effect;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        Instantiate(effect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
