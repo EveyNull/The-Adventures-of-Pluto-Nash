348331fc33ecce249077d1e419987f2b379259fc
aravash2 <Jacob2.Woodman@live.uwe.ac.uk>
2020-04-18 15:35:17 +0100

    boss room done

--- Assets/BulletStunAndDamage.cs
+++ Assets/BulletStunAndDamage.cs
@@ -7,9 +7,15 @@ public class BulletStunAndDamage : MonoBehaviour
     public bool deathEnabled = true;
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("hit something");
         if (other.CompareTag("Player"))
         {
-            other.GetComponent<playerControlLerpSmoothAnim>().Stun();
+            Debug.Log("hit something tagged player");
+            if (other.GetComponent<playerControlLerpSmoothAnim>())
+            {
+                other.GetComponent<playerControlLerpSmoothAnim>().Stun();
+            }
+
             if(deathEnabled) Destroy(transform.parent.gameObject);
         }
     }
